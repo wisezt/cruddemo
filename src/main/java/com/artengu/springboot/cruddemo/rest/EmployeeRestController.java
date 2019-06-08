2b3b4edb123829918d33388a57c7e1a1d8d69235
Ting <wisezt@hotmail.com>
2019-06-08 09:23:49 -0500

    Setup EmployeeRestController

--- src/main/java/com/artengu/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/artengu/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,4 +1,33 @@
 package com.artengu.springboot.cruddemo.rest;
 
+import com.artengu.springboot.cruddemo.dao.EmployeeDAO;
+import com.artengu.springboot.cruddemo.entity.EmployeeEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO){
+        employeeDAO = theEmployeeDAO;
+    }
+
+
+    @GetMapping("/employees'")
+    public List<EmployeeEntity> findAll(){
+        return employeeDAO.findAll();
+    }
+
+
+
+
 }
