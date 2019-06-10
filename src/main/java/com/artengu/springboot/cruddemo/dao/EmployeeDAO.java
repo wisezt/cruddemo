2a151cd26305686c56786efd744ce439f20c4424
Ting <wisezt@hotmail.com>
2019-06-09 22:24:02 -0500

    Add new stuff in EmloyeeDAo interface

--- src/main/java/com/artengu/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/artengu/springboot/cruddemo/dao/EmployeeDAO.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface EmployeeDAO {
     public List<EmployeeEntity> findAll();
+
+    public EmployeeEntity findById(int theId);
+
+    public void save(EmployeeEntity theEmployeeEntity);
+
+    public void deleteById(int theId);
 }
