7da162f499e6e989c019957c2600cd42a9b41ae7
Ting <wisezt@hotmail.com>
2019-06-10 20:16:57 -0500

    Delete DAO & Add Repository

---
+++ src/main/java/com/artengu/springboot/cruddemo/springdatajpa/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.artengu.springboot.cruddemo.springdatajpa;
+
+import com.artengu.springboot.cruddemo.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+}
