9ee2d6da7cde5110f74d77f45d397981ce1430ff
Ting <wisezt@hotmail.com>
2019-06-10 21:07:39 -0500

    Change the path employees to members

--- src/main/java/com/artengu/springboot/cruddemo/springdatajpa/EmployeeRepository.java
+++ src/main/java/com/artengu/springboot/cruddemo/springdatajpa/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.artengu.springboot.cruddemo.springdatajpa;
 import com.artengu.springboot.cruddemo.entity.Employee;
 
         import org.springframework.data.jpa.repository.JpaRepository;
-        import org.springframework.stereotype.Service;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
